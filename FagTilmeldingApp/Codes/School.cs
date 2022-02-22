9bd9678c9929e23cf04fa39aded6d2550f9b944c
kiranmayi <kiranmayi1609@gmail.com>
2022-02-22 22:35:29 +0100

    Add project files.

---
+++ FagTilmeldingApp/Codes/School.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    //abstrakt indkapsler school-Det kan være base klass(parent class)
+    internal abstract class School
+    {
+        public string schoolnavn { get; set; }
+
+        protected School(string schoolnavn)
+        {
+            this.schoolnavn = schoolnavn;
+        }
+    }
+}
---
+++ FagTilmeldingApp/Codes/Semester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{   
+    //det er sealed sidst nedarvning og semester derived class af school 
+    internal sealed class Semester:School
+    {
+        
+        public string? SemesterNavn { get;set; }
+       //Det er en konstructor -inheritance af parent class
+        public Semester(string? schoolnavn,string? semesterNavn):base(schoolnavn)
+            
+        {
+            SemesterNavn = semesterNavn;
+             
+        }
+        
+        
+     
+
+
+    }
+}
